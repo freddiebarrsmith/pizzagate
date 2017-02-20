0751a44ef0ea36536221c99ed0840e297773d830
freddie <freddie@localhost.localdomain>
2017-02-20 22:11:43 +0000

    gabe did 9/11

--- reader.py
+++ reader.py
@@ -4,3 +4,12 @@ with open("small.in") as f:
     mylist = f.read().splitlines()
 
 print(mylist[0])
+
+
+rows = mylist[0][0]
+columns = mylist[0][2]
+minsepingredient = mylist[0][4]
+maxslicesize = mylist[0][6]
+print(rows)
+print(columns)
+print(minsepingredient)
